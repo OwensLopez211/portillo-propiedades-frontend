a0d6c327d957ee118828df5d3ec8746db5c5aa7a
Owens López <111214602+OwensLopez211@users.noreply.github.com>
2024-10-18 20:41:47 +0000

    Update: Banners de secciones y correcciones adicionales

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/General/Navbar';
 import SecondaryHero from '../components/General/SecundaryHero';
 import Footer from '../components/General/Footer';
 import ServiceSection from '../components/ServicePage/ServiceSection';
+import FloatingSocialButtons from '../components/General/FloatingSocialButtons'
 
 const Services = () => {
   return (
@@ -14,36 +15,31 @@ const Services = () => {
       <SecondaryHero
         title="Servicios"
         subtitle=""
-        backgroundImage="/64.jpg"
+        backgroundImage="/ServicesBanner.jpg"
       />
 
       <div className="container mx-auto px-4 py-10 max-w-5xl">  {/* Añadido max-w-5xl para limitar el ancho máximo */}
         <ServiceSection
-          image="/juridico.jpg"
-          title="ESTUDIO JURÍDICO"
-          description="Las ventajas asociadas que entrega nuestra empresa de corretaje, es un servicio legal que está a disposición del cliente, con 2 abogados a su servicio, quienes realizan la gestión directa de cobranza extrajudicial y gestión judicial forma directa si fuera necesario, para exigir el cumplimiento de las obligaciones derivadas de los contratos de arriendo, estando incorporados estos servicios en su pago de administración. Contratos y títulos de propiedad. Servicio Judicial, Cobranza extrajudicial gratuita y más."
+          image="/Service1.png"
+          title="Valorización de su Propiedad"
+          description="Conozca el verdadero valor de su propiedad con nuestra experiencia en el mercado inmobiliario. Realizamos un análisis exhaustivo y detallado de su propiedad, considerando factores como la ubicación, el estado actual y las tendencias del mercado. Así, podrá tomar decisiones informadas sobre la venta o arrendamiento de su propiedad."
         />
         
         <ServiceSection
-          image="/compraventa.jpg"
-          title="COMPRA Y VENTA"
-          description="Porque tenemos experiencia en el área inmobiliaria, sabemos la importancia de comprar o vender tu propiedad. Para Portilla Propiedades lo importante, más allá de lograr transacciones comerciales, exitosas en la intermediación de negocios inmobiliarios, es crear lazos y relaciones comerciales permanentes con todos sus clientes, que se prolonguen en el tiempo, intentando cada vez, superar las expectativas de quienes nos eligen, para ayudarlos en el camino de vender, comprar o invertir en una propiedad."
+          image="/Service2.png"
+          title="Corretaje de Propiedades"
+          description="Deje que nuestros expertos se encarguen de la venta o arriendo de su propiedad. Ofrecemos un servicio integral que incluye la promoción, gestión de visitas y negociación, asegurando que obtenga el mejor trato. Nos dedicamos a encontrar el comprador o arrendatario perfecto para su propiedad, facilitando cada paso del proceso."
         />
         
         <ServiceSection
-          image="/administracion.jpg"
-          title="ADMINISTRACIÓN DE PROPIEDADES"
-          description="En Portilla Propiedades entendemos lo que significa para usted arrendar una propiedad. Tenemos un sistema y equipo de personas especializado para ofrecerle toda la asistencia integral necesaria durante todo el proceso para brindarle toda la tranquilidad que necesita. Nos encargamos del cobro de arriendo mensual, coordinamos todas las reparaciones necesarias, controlamos el pago de servicios y gastos comunes y revisamos periódicamente el estado de la propiedad."
-        />
-        
-        <ServiceSection
-          image="/apartamoblados.jpg"
-          title="APART AMOBLADOS"
-          description="Somos líderes en el arriendo de cómodos y modernos departamentos ubicados en Las Condes, Santiago de Chile. Ofrecemos arriendo por día y también con convenio, y servicios especiales para largas estadías. Lugares ideales para su visita de negocios. Ubicaciones excelentes y con excelente conectividad. Comuníquese hoy mismo y con gusto le ofreceremos las distintas opciones que tenemos para usted."
+          image="/Service3.png"
+          title="Preparación y Mantención de Propiedades"
+          description="Transforme su propiedad en un espacio atractivo y funcional con nuestros servicios de mantención. Nos encargamos de todo, desde pintura y limpieza hasta jardinería y cerrajería. Prepararemos su propiedad para la venta o arriendo, garantizando que destaque en el mercado y capte la atención de potenciales compradores o arrendatarios."
         />
 
         {/* Añade más secciones según sea necesario */}
       </div>
+      <FloatingSocialButtons />
 
       <Footer />
     </div>
