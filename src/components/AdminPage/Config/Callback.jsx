cae994aa415ee2a247a28ef2dfce2b7bf480e55e
Owens <owenslopez211@gmail.com>
2024-10-24 03:50:58 -0300

    integrando autenticacion con ml 2

--- src/components/AdminPage/Config/Callback.jsx
+++ src/components/AdminPage/Config/Callback.jsx
@@ -9,7 +9,7 @@ const Callback = () => {
     const authorizationCode = urlParams.get('code');
     const state = urlParams.get('state');
 
-    if (authorizationCode && state === 'expected_state_value') {
+    if (authorizationCode) {
       // Enviar el código al backend para intercambiarlo por el token
       fetch('https://portillo-propiedades-backend.onrender.com/mercadolibre/get-token/', {
         method: 'POST',
@@ -18,7 +18,7 @@ const Callback = () => {
         },
         body: JSON.stringify({
           code: authorizationCode,
-          code_verifier: localStorage.getItem('code_verifier'), // Recuperar el code_verifier almacenado en localStorage
+          code_verifier: localStorage.getItem('code_verifier'), // Asegúrate de que esto tiene un valor válido
         }),
       })
         .then(response => response.json())
@@ -26,7 +26,10 @@ const Callback = () => {
           if (data.access_token) {
             // Guardar el token en el localStorage o en un estado global
             localStorage.setItem('access_token', data.access_token);
-            navigate('/admin'); // Redirigir al panel de administración o donde quieras
+            // Redirigir al panel de administración
+            navigate('/admin');
+          } else {
+            console.error('Error en la autenticación:', data);
           }
         })
         .catch(error => {
