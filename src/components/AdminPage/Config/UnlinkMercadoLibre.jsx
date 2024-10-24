d298e58caa8b8e5675ee5bef1291840540778e56
Owens <owenslopez211@gmail.com>
2024-10-24 14:49:37 -0300

    Agregando UnlinkMercadolibre

--- src/components/AdminPage/Config/UnlinkMercadoLibre.jsx
+++ src/components/AdminPage/Config/UnlinkMercadoLibre.jsx
@@ -12,20 +12,29 @@ const UnlinkMercadoLibre = () => {
 
     console.log('Desincronizado de MercadoLibre.');
 
-    // Redirige al usuario a la página de configuración
-    navigate('/admin/configuracion');
+    // Redirigir al usuario al logout de MercadoLibre
+    const mercadoLibreLogoutURL = `https://www.mercadolibre.com/jms/mlc/lgz/logout`;
+
+    // Redirigir al usuario al logout de MercadoLibre
+    window.location.href = mercadoLibreLogoutURL;
+
+    // Después del logout, redirigir al usuario a la página de configuración
+    setTimeout(() => {
+      navigate('/admin/configuracion');
+    }, 2000);  // Espera unos segundos para que se complete el logout
   };
 
   return (
     <div className="flex justify-center mt-8">
       <button
-        onClick={handleUnlink}
+        onClick={handleUnlink}  // Mantén solo este onClick
         className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-lg shadow-md"
       >
         Desvincular MercadoLibre
       </button>
     </div>
   );
+  
 };
 
 export default UnlinkMercadoLibre;
