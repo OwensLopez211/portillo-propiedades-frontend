38c747ea1d777aa91a08ab29f6518e42780493f7
Owens <owenslopez211@gmail.com>
2024-10-25 13:59:08 -0300

    Update: Separando componentes PropertyPage

--- src/components/PropertiesPage/PropertyList.jsx
+++ src/components/PropertiesPage/PropertyList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropertyCard from './PropertyPageCards'; // Importa el componente PropertyCard
+import PropertyCards from './PropertyPageCards'; // Importa el componente PropertyCard
 
 const PropertyList = ({ properties }) => {
     if (!properties || properties.length === 0) {
@@ -10,7 +10,7 @@ const PropertyList = ({ properties }) => {
         <div className="container mx-auto p-4">
             <div className="property-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {properties.map((property) => (
-                <PropertyCard key={property.id} property={property} />
+                <PropertyCards key={property.id} property={property} />
             ))}
             </div>
         </div>
