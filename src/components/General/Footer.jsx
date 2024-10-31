af91868fa4bd3d8497663c474a0b6a84bb6d4f64
Owens <owenslopez211@gmail.com>
2024-10-31 15:16:29 -0300

    agregando redes en footer

--- src/components/General/Footer.jsx
+++ src/components/General/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {
   FaEnvelope,
   FaPhoneAlt,
+  FaInstagram,
+  FaFacebookF,
+  FaTiktok,
+  FaFacebookMessenger,
 } from 'react-icons/fa';
 
 const Footer = () => {
@@ -20,20 +24,56 @@ const Footer = () => {
         {/* Contacto - Derecha */}
         <div className="lg:w-1/3 text-center lg:text-right">
           <h3 className="font-bold text-2xl mb-4">CONTACTO</h3>
-          <ul className="space-y-3">
-            <li className="flex justify-start lg:justify-end items-center">
-              <FaEnvelope className="mr-2 text-xl" />
-              <a href="mailto:marketing@portillapropiedades.cl" className="hover:underline hover:text-gray-300 transition duration-300 ml-2 text-lg">
+          <ul className="space-y-3 flex flex-col items-start lg:items-end">
+            <li className="flex items-center">
+              <FaEnvelope className="text-xl" />
+              <a href="mailto:contacto@newlandpropiedades.cl" className="hover:underline hover:text-gray-300 transition duration-300 ml-2 text-lg">
                 Contacto@newlandpropiedades.cl
               </a>
             </li>
-            <li className="flex justify-start lg:justify-end items-center">
-              <FaPhoneAlt className="mr-2 text-xl" />
+            <li className="flex items-center">
+              <FaPhoneAlt className="text-xl" />
               <a href="tel:+56 9 9847 2202" className="hover:underline hover:text-gray-300 transition duration-300 ml-2 text-lg">
                 +56 9 9847 2202
               </a>
             </li>
           </ul>
+
+          {/* Redes Sociales */}
+          <div className="flex justify-center lg:justify-end mt-4 space-x-4">
+            <a
+              href="https://www.instagram.com/newland_propiedades_/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-gray-300 transition duration-300"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/newland_propiedades"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-gray-300 transition duration-300"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.tiktok.com/@newland_propiedades"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-gray-300 transition duration-300"
+            >
+              <FaTiktok />
+            </a>
+            <a
+              href="https://www.messenger.com/t/newland_propiedades"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-gray-300 transition duration-300"
+            >
+              <FaFacebookMessenger />
+            </a>
+          </div>
         </div>
 
       </div>
