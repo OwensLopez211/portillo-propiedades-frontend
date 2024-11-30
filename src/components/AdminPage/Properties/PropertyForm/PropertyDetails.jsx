976b2834f5d51e0cc3a3325934f8518155e018f6
Owens <owenslopez211@gmail.com>
2024-11-30 03:20:04 -0300

    Commit: cambios

--- src/components/AdminPage/Properties/PropertyForm/PropertyDetails.jsx
+++ src/components/AdminPage/Properties/PropertyForm/PropertyDetails.jsx
@@ -10,7 +10,7 @@ const PropertyDetails = ({ formData, handleChange, handleCurrencyChange, handleP
     // Llama al endpoint del backend para obtener el valor actual de la UF
     const fetchUF = async () => {
       try {
-        const response = await axios.get(`${API_BASE_URL}/api/uf/`); // Endpoint del backend
+        const response = await axios.get(`${REACT_APP_API_URL}/api/uf/`); // Endpoint del backend
         setValorUF(response.data.valor_uf);
         setLoadingUF(false);
       } catch (error) {
